4a888eea166a746cb91c6631dec7b4308d4376d0
ManitSinghal2004 <97463501+ManitSinghal2004@users.noreply.github.com>
2022-10-22 13:34:30 +0530

    scss added

--- index.js
+++ index.js
@@ -8,6 +8,15 @@
  const passport = require('passport');
  const passportLocal = require('./config/passport-local-strategy');
  const MongoStore = require('connect-mongo')(session);
+ const sassMiddleware = require('node-sass-middleware') ;
+
+ app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css' ,
+    debug: true ,
+    outputStyle: 'extended' ,
+    prefix: '/css' 
+ }))
 
 app.set('layout extractStyles' , true) ; 
 app.set('layout extractScripts' , true) ; 
